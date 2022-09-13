a6e077af51eed34533218fcbf72d70dae37d3471
KirilovskihDenis <po128-kirilovskih@urtt.ru>
2022-09-13 09:30:23 +0300

    6_1

--- pr6_1/pr6_1/urls.py
+++ pr6_1/pr6_1/urls.py
@@ -1,4 +1,4 @@
-"""Project URL Configuration
+"""pr6_1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts import views
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomePageView.as_view(), name='home'),
+    path('', views.BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
 ]
--- pr6_1/pr6_1/wsgi.py
+++ pr6_1/pr6_1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Project project.
+WSGI config for pr6_1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pr6_1.settings')
 
 application = get_wsgi_application()
