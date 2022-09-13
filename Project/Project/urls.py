2677f443ed7bf8aef255e825b1f92bf5e9b18519
KirilovskihDenis <po128-kirilovskih@urtt.ru>
2022-09-13 09:34:31 +0300

    Revert "6_1"
    This reverts commit a6e077af51eed34533218fcbf72d70dae37d3471.

--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -1,4 +1,4 @@
-"""pr6_1 URL Configuration
+"""Project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog import views
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.BlogListView.as_view(), name='home'),
-    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
+    path('', views.HomePageView.as_view(), name='home'),
 ]
